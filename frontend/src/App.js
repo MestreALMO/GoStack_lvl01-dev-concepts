a7ce997f9f71e276bc0a6d48d7a72046881b5e70
MestreALMO <lusegardis@gmail.com>
2020-08-20 00:09:22 -0300

    removing double line and deleting image that wasn't being used

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
 import './App.css';
-import Api from './services/api';
 import api from './services/api';
 
 export default function App() {
