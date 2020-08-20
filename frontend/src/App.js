d7bacfd947da49ba85eb75b8d8a9dc12ec646ea5
MestreALMO <lusegardis@gmail.com>
2020-08-19 22:13:07 -0300

    Frontend with ReactJS concluded

---
+++ frontend/src/App.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+import Header from './components/Header';
+import './App.css';
+import Api from './services/api';
+import api from './services/api';
+
+export default function App() {
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        api.get('projects').then(response => {
+            setProjects(response.data);
+        });
+    }, []);
+
+    async function handleAddProject() {
+        // projects.push(`Novo projeto ${Date.now()}`);
+        
+        const response = await api.post('projects', {
+            title: `Novo projeto ${Date.now()}`,
+            owner: 'André Lusegardis'
+        });
+
+        const project = response.data;
+
+        setProjects([... projects, project]);
+    }
+
+    return (
+        <>
+        <Header title="Projects" />
+
+        <ul>
+            {projects.map(project => <li key={project.id}>{project.title}</li>)}
+        </ul>
+
+        <button type="button" onClick={handleAddProject}>Add project</button>
+        </>
+    );
+}
